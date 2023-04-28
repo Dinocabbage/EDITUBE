12a8d4d7842e24da9569b04b206b87a301f87c98
Dinocabbage <power0709@naver.com>
2023-04-28 11:29:54 +0900

    project start

---
+++ src/main/java/team_i/EDITUBE/EditubeApplication.java
@@ -0,0 +1,13 @@
+package team_i.EDITUBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EditubeApplication.class, args);
+	}
+
+}
